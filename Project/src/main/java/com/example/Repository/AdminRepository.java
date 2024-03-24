74252135d726bf7608bb6b6061fca1080fc64418
Elysium33 <hardalau.andrei@yahoo.com>
2024-03-24 21:24:35 +0200

    added repository classes for the users

---
+++ Project/src/main/java/com/example/Repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package com.example.Repository;
+
+import com.example.Entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+}
+
---
+++ Project/src/main/java/com/example/Repository/TrainingPlanRepository.java
@@ -0,0 +1,10 @@
+package com.example.Repository;
+
+import com.example.Entity.TrainingPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainingPlanRepository extends JpaRepository<TrainingPlan, Long> {
+
+}
